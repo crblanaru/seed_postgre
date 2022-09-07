1bd2c6d1e7283373146919b0c75d370bb8680aa1
cristianbl <crblanaru@gmail.com>
2022-09-07 11:43:13 +0200

    used ints in logic

--- scripts/db/init.sh
+++ scripts/db/init.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-psql -U $POSTGRES_USER -d $POSTGRES_DB -a -f /app/scripts/db/dump.sql
+TABLE=tbl_init_status
+SQL_EXISTS="SELECT (EXISTS (SELECT FROM pg_tables WHERE schemaname = 'public' AND tablename  = '$TABLE'))::int;"
+STATUS=$( psql -U $POSTGRES_USER -d $POSTGRES_DB -c "$SQL_EXISTS" --pset footer -t )
+
+# making sure that we do not run the script if the DB was already initialized
+if [ "$STATUS" -eq 0 ]; then
+    psql -U $POSTGRES_USER -d $POSTGRES_DB -a -f /app/scripts/db/dump.sql
+else
+    echo "DB already initialized"
+fi
