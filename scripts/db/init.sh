00676325ee40b77b1bf592a616991fdb19144c27
cristianbl <crblanaru@gmail.com>
2022-09-07 11:33:09 +0200

    simplified commit

--- scripts/db/init.sh
+++ scripts/db/init.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
 
-TABLE=tbl_init_status
-SQL_EXISTS="SELECT EXISTS (SELECT FROM pg_tables WHERE schemaname = 'public' AND tablename  = '$TABLE');"
-STATUS=$( psql -U $POSTGRES_USER -d $POSTGRES_DB -c "$SQL_EXISTS" --pset footer -t )
-
-# making sure that we do not run the script if the DB was already initialized
-if [[ "$STATUS" == *"f"* ]]; then
-    psql -U $POSTGRES_USER -d $POSTGRES_DB -a -f /app/scripts/db/dump.sql
-else
-    echo "DB already initialized"
-fi
+psql -U $POSTGRES_USER -d $POSTGRES_DB -a -f /app/scripts/db/dump.sql
