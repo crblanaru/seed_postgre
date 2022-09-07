b38344182d6818e898e70db009752575d7dd74e6
cristianbl <crblanaru@gmail.com>
2022-09-07 10:47:46 +0200

    added init chck for volume use-case

--- scripts/db/init.sh
+++ scripts/db/init.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-psql -U $POSTGRES_USER -d $POSTGRES_DB -a -f /app/scripts/db/dump.sql
+TABLE=tbl_init_status
+SQL_EXISTS="SELECT EXISTS (SELECT FROM pg_tables WHERE schemaname = 'public' AND tablename  = '$TABLE');"
+STATUS=$( psql -U $POSTGRES_USER -d $POSTGRES_DB -c "$SQL_EXISTS" --pset footer -t )
+
+# making sure that we do not run the script if the DB was already initialized
+if [ $STATUS -eq "f" ]; then
+    psql -U $POSTGRES_USER -d $POSTGRES_DB -a -f /app/scripts/db/dump.sql
+else
+    echo "DB already initialized"
+fi
